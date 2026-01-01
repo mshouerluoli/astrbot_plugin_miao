2debc64202e7e9fb602956e87d77b401eedd1a54
喵帕斯 <2824779102@qq.com>
2026-01-02 05:57:07 +0800

    20260102

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+@register("astrbot_plugin_miao", "miao", "AstrBot 插件示例", "v1.1.1")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)

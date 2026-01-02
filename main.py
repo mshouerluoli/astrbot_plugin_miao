ce4e7f22a19f00dc269e2313acb1070ce6f1739b
喵帕斯 <2824779102@qq.com>
2026-01-02 21:02:55 +0800

    屏蔽helloworld

--- main.py
+++ main.py
@@ -18,14 +18,14 @@ class MyPlugin(Star):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+    # @filter.command("helloworld")
+    # async def helloworld(self, event: AstrMessageEvent):
+    #     """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+    #     user_name = event.get_sender_name()
+    #     message_str = event.message_str # 用户发的纯文本消息字符串
+    #     message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
+    #     logger.info(message_chain)
+    #     yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
     
 
 
@@ -38,16 +38,14 @@ class MyPlugin(Star):
         
         pattern = r'(?=.*胡桃)(?=.*http)'
         if re.search(pattern, message_str, re.DOTALL):
+            qq_value = self.config.get("HuTao_config",0)
+            if qq_value !=0:
+                chain = [
+                    Comp.At(qq=qq_value),
+                    Comp.Plain(" 发现胡桃链接,嗷~"),
+                ]
+                yield event.chain_result(chain)
 
-            hu_tao_config = self.config.get("HuTao_config")
-            qq_value = hu_tao_config.get("QQ")  # 假设键为"value"
-            yield event.plain_result(f"发现胡桃链接,嗷~ {qq_value}")
-            # chain = [
-            #     Comp.At(qq=1969207693),
-            #     Comp.Plain(" 发现胡桃链接,嗷~"),
-            # ]
-            # yield event.chain_result(chain)
-            #yield event.plain_result(f"发现胡桃链接,嗷~ {event.get_sender_id()}")
 
 
 
